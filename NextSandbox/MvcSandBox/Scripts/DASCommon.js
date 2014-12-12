de045e27f1f1eabbe53c944e71083d342a642f99
Squallygator <pascal.menardais@gmail.com>
2014-12-12 15:13:16 +0100

    MAJ

---
+++ NextSandbox/MvcSandBox/Scripts/DASCommon.js
@@ -0,0 +1,50 @@
+﻿/// <reference path="../jquery-1.11.0.js" />
+
+// -------------------------------------------------------------------------------------
+// String Prototypes
+// -------------------------------------------------------------------------------------
+String.prototype.DASContains = function (s) {
+    return (this.indexOf(s) !== -1);
+};
+
+// -------------------------------------------------------------------------------------
+// JQuery Extensions Plugins
+// -------------------------------------------------------------------------------------
+(function ($) {
+    jQuery.fn.DASValue = function (value) {
+        var accesseur = $(this).is('input, select, textarea') ? 'val' : 'html';
+        if ($(this)[accesseur]() == null || $(this)[accesseur]() == "") return null;
+        if (value != null) {
+            $(this)[accesseur](value);
+            return value;
+        }
+        else {
+            return $(this)[accesseur]();
+        }
+    };
+})(jQuery);
+
+// -------------------------------------------------------------------------------------
+// Number Prototypes
+// -------------------------------------------------------------------------------------
+Number.prototype.DASRound = function (len) {
+    var num = this;
+    if (len == null || len < 0) len = 0;
+    var multiplicateur = Math.pow(10, len);
+    return Math.round(num * multiplicateur) / multiplicateur;
+};
+
+// -------------------------------------------------------------------------------------
+// Basic Functions
+// -------------------------------------------------------------------------------------
+zeroOrValue = function (value) {
+    if (value == null || /^\s*$/.test(value)) {
+        return 0;
+    } else {
+        if (!(value.replace))
+            return value;
+        else {
+            return parseFloat(value.replace(",", "."));
+        }
+    }
+};
--- NextSandbox/MvcSandBox/Scripts/DasAjaxReload.js
+++ NextSandbox/MvcSandBox/Scripts/DasAjaxReload.js
@@ -1,83 +1,126 @@
-﻿/// <reference path="~/Scripts/jquery-2.1.0.js" />
+﻿/// <reference path="~/Scripts/Views/Shared/notification.js" />
+/// <reference path="~/Scripts/jquery-2.1.1.intellisense.js" />
+/// <reference path="~/Scripts/jquery-2.1.0-vsdoc.js" />
+
 var DasAjaxReloadOptions = function () {
     this.targetSelector = null;
     this.ajaxUrl = null;
+    this.ajaxType = 'POST';
+    this.ajaxDataType = 'html';
     this.GetDataDelegate = null;
     this.triggerChangeSeletors = null;
     this.delegateBeforeAjax = null;
     this.delegateSuccess = null;
     this.delegateError = null;
+    this.execute = false;
+    //var that = this;
+    //this.GetAjaxUrl = function () {
+    //    var ajaxUrl = that.ajaxUrl;
+    //    if (!ajaxUrl) return null;
+    //    if (typeof ajaxUrl === 'function')
+    //        return ajaxUrl();
+    //    return ajaxUrl;
+    //};
+    //this.GetData = function () {
+    //    var dataDelegate = that.GetDataDelegate;
+    //    if (!dataDelegate || typeof dataDelegate !== 'function')
+    //        return $(that.targetSelector).closest('form').serialize();
+    //    return dataDelegate();
+    //};
+    //this.beforeAjax = function (eventThis) {
+    //    if (that.delegateBeforeAjax && typeof that.delegateBeforeAjax === 'function')
+    //        that.delegateBeforeAjax(eventThis);
+    //};
+    //this.done = function (html) {
+    //    if ($(that.targetSelector))
+    //        $(that.targetSelector).html(html);
+    //    if (that.delegateSuccess && typeof that.delegateSuccess === 'function')
+    //        that.delegateSuccess(html);
+    //};
+    //this.fail = function (xhr) {
+    //    console.log('erreur AJAX DasAjaxReload : ' + JSON.stringify(xhr));
+    //    Notification.Create("Erreur AJAX DasAjaxReload", Notification.Ko);
+    //    if (that.delegateError && typeof that.delegateError === 'function')
+    //        that.delegateError(xhr);
+    //};
+};
+var DasAjaxReload = function ($container, dasAjaxReloadOptions) {
+    if (!dasAjaxReloadOptions) return;
+    var options = (dasAjaxReloadOptions instanceof DasAjaxReloadOptions) ? dasAjaxReloadOptions : $.extend(true, new DasAjaxReloadOptions(), dasAjaxReloadOptions);
+
+    if (!options.ajaxUrl) return;
+    if (!$(options.targetSelector)) return;
+    if (!options.triggerChangeSeletors) return;
     var that = this;
+
     this.GetAjaxUrl = function () {
-        var ajaxUrl = that.ajaxUrl;
+        var ajaxUrl = options.ajaxUrl;
         if (!ajaxUrl) return null;
         if (typeof ajaxUrl === 'function')
             return ajaxUrl();
         return ajaxUrl;
     };
+
     this.GetData = function () {
-        var dataDelegate = that.GetDataDelegate;
+        var dataDelegate = options.GetDataDelegate;
         if (!dataDelegate || typeof dataDelegate !== 'function')
-            return $(that.targetSelector).closest('form').serialize();
+            return $(options.targetSelector).closest('form').serialize();
         return dataDelegate();
     };
+
     this.beforeAjax = function (eventThis) {
-        console.log('DasAjaxReloadObject.beforeAjax data : ' + JSON.stringify(data));
-        if (that.delegateBeforeAjax && typeof that.delegateBeforeAjax === 'function')
-            that.delegateBeforeAjax(eventThis);
+        if (options.delegateBeforeAjax && typeof options.delegateBeforeAjax === 'function')
+            options.delegateBeforeAjax(eventThis);
     };
+
     this.done = function (html) {
-        if ($(options.targetSelector))
+        if ($(options.targetSelector)) {
+            $(options.targetSelector).empty();
             $(options.targetSelector).html(html);
-        if (that.delegateSuccess && typeof that.delegateSuccess === 'function')
-            that.delegateSuccess(html);
+        }
+        if (options.delegateSuccess && typeof options.delegateSuccess === 'function')
+            options.delegateSuccess(html);
     };
+
     this.fail = function (xhr) {
         console.log('erreur AJAX DasAjaxReload : ' + JSON.stringify(xhr));
-        if (that.delegateError && typeof that.delegateError === 'function')
-            that.delegateError(xhr);
+        Notification.Create("Erreur AJAX DasAjaxReload", Notification.Ko);
+        if (options.delegateError && typeof options.delegateError === 'function')
+            options.delegateError(xhr);
     };
+
+    this.reload = function () {
+        that.beforeAjax(this);
+        var url = that.GetAjaxUrl();
+        if (!url) return;
+        console.log(this.id + ' has triggered a DasAjaxReload for ' + url);
+        var data = that.GetData();
+        $.ajax(url, {
+            type: options.ajaxType,
+            dataType: options.ajaxDataType,
+            cache: false,
+            data: data,
+        })
+        .done(that.done)
+        .fail(that.fail);
+    };
+
+    if (!options.triggerChangeSeletors && !options.execute) return;
+    var triggerChangeSeletors = options.triggerChangeSeletors;
+    var length = triggerChangeSeletors.length;
+    for (var i = 0; i < length; i++) {
+        var selector = triggerChangeSeletors[i];
+        var eventName = 'change';
+        if ($(selector).data('dasAutocompleteText'))
+            eventName = 'autocompleteChange';
+        $container.on(eventName, selector, that.reload);
+    }
+    if (options.execute)
+        that.reload();
 };
 jQuery.fn.extend({
-    //    DasAjaxReload: function (targetSelector, ajaxUrl, triggerChangeSeletors, delegateSuccess) {
-    //        if (!$(targetSelector)) return;
-    //        if (!ajaxUrl) return;
-    //        if (!triggerChangeSeletors) return;
-    //        var options = new DasAjaxReloadOptions();
-    //        options.targetSelector = targetSelector;
-    //        options.ajaxUrl = ajaxUrl;
-    //        options.triggerChangeSeletors = triggerChangeSeletors;
-    //        options.delegateSuccess = delegateSuccess;
-    //        $(this).DasAjaxReloadObject(options);
-    //},
     DasAjaxReload: function (dasAjaxReload) {
-        if (!dasAjaxReload) return;
-        var options = (dasAjaxReload instanceof DasAjaxReloadOptions) ? dasAjaxReload : $.extend(true, new DasAjaxReloadOptions(), dasAjaxReload);
-        if (!options.ajaxUrl) return;
-        if (!$(options.targetSelector)) return;
-        if (!options.triggerChangeSeletors) return;
-        var reload = function () {
-            options.beforeAjax(this);
-            var url = options.GetAjaxUrl();
-            var data = options.GetData();
-            $.ajax(url, {
-                type: 'POST',
-                dataType: 'html',
-                data: data,
-            })
-            .done(options.done)
-            .fail(options.fail);
-        };
-        var $this = $(this);
-        if (!options.triggerChangeSeletors) return;
-        var triggerChangeSeletors = options.triggerChangeSeletors;
-        var length = triggerChangeSeletors.length;
-        for (var i = 0; i < length; i++) {
-            var selector = triggerChangeSeletors[i];
-            var eventName = 'change';
-            if ($(selector).data('dasAutocompleteText'))
-                eventName = 'autocompleteChange';
-            $this.on(eventName, selector, reload);
-        }
+        if (!dasAjaxReload) return undefined;
+        return new DasAjaxReload($(this), dasAjaxReload);
     }
 });
