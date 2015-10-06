5fa78a550bafacddab540e9b265bbe35f16d6d75
Squallygator <pascal.menardais@gmail.com>
2015-10-06 16:14:09 +0200

    Creation et premier tests

---
+++ NextSandbox/AngularSandbox/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AngularSandbox")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AngularSandbox")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("721aeec6-2136-49c6-a939-e7e272da623e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NextSandbox/AngularSandbox/Scripts/VenteVO/logistique.js
@@ -0,0 +1,59 @@
+﻿angular.module('logistique', [])
+  .controller('logistiqueController', function () {
+      var logistique = this;
+      logistique.ddl = {
+          EtatTransport: [
+              { value: '', text: '' },
+              { value: '1', text: 'Mise à dispo. gratuite' },
+              { value: '2', text: 'Mise à dispo. payante' },
+              { value: '3', text: 'Transport gratuit' },
+              { value: '4', text: 'Transport payant' }
+          ]
+      };
+      logistique.EtatTransportChange = function (vehiculeId) {
+          var currentLine;
+          for (var i = 0; i < logistique.details.length; i++) {
+              if (logistique.details[i].VehiculeId === vehiculeId) {
+                  currentLine = logistique.details[i];
+                  break;
+              }
+          }
+          if (currentLine) {
+              alert('EtatTransportChange changed for ' + currentLine.VehiculeId);
+          }
+      };
+      logistique.VilleLivraisonChange = function (vehiculeId) {
+          var currentLine;
+          for (var i = 0; i < logistique.details.length; i++) {
+              if (logistique.details[i].VehiculeId === vehiculeId) {
+                  currentLine = logistique.details[i];
+                  break;
+              }
+          }
+          if (currentLine) {
+              alert('VilleLivraison changed for ' + currentLine.VehiculeId);
+          }
+      };
+    logistique.details = [
+        {
+            VehiculeId: 38211,
+            VehiculeLink: '/Vehicule/38211',
+            Stockage: 'HN Autotransport nv',
+            VilleStockage: 'B3700 TONGEREN',
+            EtatTransport: '3',
+            VilleLivraison: 1,
+            VillesLivraison: [
+                { value: 1, text: '32108 - BAD SALZUFLEN' },
+                { value: '2', text: '32108 - BAD SALZUFLEN 2' }
+            ]
+        },
+        {
+            VehiculeId: 456,
+            VehiculeLink: '/Vehicule/456',
+            Stockage: 'Stockage',
+            VilleStockage: 'VilleStockage',
+            EtatTransport: '4',
+            VilleLivraison: [{ value: '1', text: '32108 - BAD SALZUFLEN', selected: true }]
+        }
+    ];
+});
