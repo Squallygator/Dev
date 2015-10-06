67c5358632e2098d6b1d3963da822c171ece6430
Squallygator <pascal.menardais@gmail.com>
2015-10-06 18:14:42 +0200

    utilisation de $resource

--- NextSandbox/AngularSandbox/Api/Ddl/EtatTransport.js
+++ NextSandbox/AngularSandbox/Api/Ddl/EtatTransport.js
@@ -1,8 +1,7 @@
-﻿{
-    data: [
-       { value: '', text: '' },
-       { value: '1', text: 'Mise à dispo. gratuite' },
-       { value: '2', text: 'Mise à dispo. payante' },
-       { value: '3', text: 'Transport gratuit' },
-       { value: '4', text: 'Transport payant' }]
-}
+﻿[
+    { "value": "", "text": "" },
+    { "value": "1", "text": "Mise à dispo. gratuite" },
+    { "value": "2", "text": "Mise à dispo. payante" },
+    { "value": "3", "text": "Transport gratuit" },
+    { "value": "4", "text": "Transport payant" }
+]
--- NextSandbox/AngularSandbox/Scripts/VenteVO/logistique.js
+++ NextSandbox/AngularSandbox/Scripts/VenteVO/logistique.js
@@ -1,69 +1,70 @@
-﻿angular.module('logistique', [])
-  .controller('logistiqueController', ['$scope', '$http', function ($scope, $http) {
-      $scope.EtatTransportChange = function (vehiculeId) {
-          var currentLine;
-          for (var i = 0; i < $scope.details.length; i++) {
-              if ($scope.details[i].VehiculeId === vehiculeId) {
-                  currentLine = $scope.details[i];
-                  break;
-              }
-          }
-          if (currentLine) {
-              alert('EtatTransportChange changed for ' + currentLine.VehiculeId);
-          }
-      };
-      $scope.VilleLivraisonChange = function (vehiculeId) {
-          var currentLine;
-          for (var i = 0; i < $scope.details.length; i++) {
-              if ($scope.details[i].VehiculeId === vehiculeId) {
-                  currentLine = $scope.details[i];
-                  break;
-              }
-          }
-          if (currentLine) {
-              alert('VilleLivraison changed for ' + currentLine.VehiculeId);
-          }
-      };
-      $scope.Ville
-      $scope.ddl = {
-          EtatTransport: [
-              { value: '', text: '' },
-              { value: '1', text: 'Mise à dispo. gratuite' },
-              { value: '2', text: 'Mise à dispo. payante' },
-              { value: '3', text: 'Transport gratuit' },
-              { value: '4', text: 'Transport payant' }
-          ]
-      };
-      $http({method: 'GET', url: "http://localhost/AngularSandbox/Api/Ddl/EtatTransport.js", responseType: "json"}).
-        then(function (response) {
-          debugger;
-            $scope.ddl.EtatTransport = response;
-        }, function (response) {
-            debugger;
-            alert("Request failed");
-        });
-      $scope.VenteVOId = 1;
-      $scope.EntiteId = 123;
-      $scope.details = [
-        {
-            VehiculeId: 38211,
-            VehiculeLink: '/Vehicule/38211',
-            Stockage: 'HN Autotransport nv',
-            VilleStockage: 'B3700 TONGEREN',
-            EtatTransport: '3',
-            VilleLivraison: 1,
-            VillesLivraison: [
-                { value: 1, text: '32108 - BAD SALZUFLEN' },
-                { value: '2', text: '32108 - BAD SALZUFLEN 2' }
-            ]
-        },
-        {
-            VehiculeId: 456,
-            VehiculeLink: '/Vehicule/456',
-            Stockage: 'Stockage',
-            VilleStockage: 'VilleStockage',
-            EtatTransport: '4',
-            VilleLivraison: [{ value: '1', text: '32108 - BAD SALZUFLEN', selected: true }]
-        }
-      ];
+﻿/// <reference path="../angular.js" />
+/// <reference path="../angular-resource.js" />
+var refs = refs || {};
+refs.EtatsTransport = refs.EtatsTransport || 'EtatsTransport';
+var refUrls = refUrls || {};
+refUrls.EtatsTransport = refUrls.EtatsTransport || 'http://localhost/AngularSandbox/Api/Ddl/EtatTransport.js';
+
+var refServices = angular.module('refServices', ['ngResource']);
+refServices.factory(refs.EtatsTransport, ['$resource',
+  function ($resource) {
+      return $resource(refUrls.EtatsTransport, {}, {
+          query: { method: 'GET', isArray: true }
+      });
   }]);
+
+var logistiqueModule = angular.module('logistique', ['refServices']);
+logistiqueModule.controller('logistiqueController', ['$scope', refs.EtatsTransport, function ($scope, etatsTransport) {
+    $scope.EtatTransportChange = function (vehiculeId) {
+        var currentLine;
+        for (var i = 0; i < $scope.details.length; i++) {
+            if ($scope.details[i].VehiculeId === vehiculeId) {
+                currentLine = $scope.details[i];
+                break;
+            }
+        }
+        if (currentLine) {
+            alert('EtatTransportChange changed for ' + currentLine.VehiculeId);
+        }
+    };
+    $scope.VilleLivraisonChange = function (vehiculeId) {
+        var currentLine;
+        for (var i = 0; i < $scope.details.length; i++) {
+            if ($scope.details[i].VehiculeId === vehiculeId) {
+                currentLine = $scope.details[i];
+                break;
+            }
+        }
+        if (currentLine) {
+            alert('VilleLivraison changed for ' + currentLine.VehiculeId);
+        }
+    };
+    $scope.ddl = {
+        EtatTransport: []
+    };
+    $scope.ddl.EtatTransport = etatsTransport.query();
+    $scope.VenteVOId = 1;
+    $scope.EntiteId = 123;
+    $scope.details = [
+      {
+          VehiculeId: 38211,
+          VehiculeLink: '/Vehicule/38211',
+          Stockage: 'HN Autotransport nv',
+          VilleStockage: 'B3700 TONGEREN',
+          EtatTransport: '3',
+          VilleLivraison: 1,
+          VillesLivraison: [
+              { value: 1, text: '32108 - BAD SALZUFLEN' },
+              { value: '2', text: '32108 - BAD SALZUFLEN 2' }
+          ]
+      },
+      {
+          VehiculeId: 456,
+          VehiculeLink: '/Vehicule/456',
+          Stockage: 'Stockage',
+          VilleStockage: 'VilleStockage',
+          EtatTransport: '4',
+          VilleLivraison: [{ value: '1', text: '32108 - BAD SALZUFLEN', selected: true }]
+      }
+    ];
+}]);
