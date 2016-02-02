bcf994ad48d5845936b668b1cc6de2902dc8105a
Squallygator <pascal.menardais@gmail.com>
2016-02-02 14:28:15 +0100

    Test sur un autre PC

---
+++ SurfCalendar/Business/SpotBusiness.cs
@@ -0,0 +1,24 @@
+﻿using SurfCalendar.Models;
+using System.Collections.Generic;
+using System;
+
+namespace SurfCalendar.Business
+{
+    public class SpotBusiness : ISpotBusiness
+    {
+        public void Create(Spot spot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Spot GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Spot> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
